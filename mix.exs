82cd0c66eb9a3c2185a3daa29777ff73abd07e71
Chad Woolley <thewoolleyman@gmail.com>
2017-06-11 01:32:10 -0700

    works

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule CfPhoenix.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
+#     {:phoenix_ecto, "~> 3.0"},
 #     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
